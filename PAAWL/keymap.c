b0729c6217241e6fb2e9041148786a69e71d6250
Jacob Lamont <cob@jacoblamont.com>
2026-02-11 10:02:40 -0500

    attemp 1.2mil

--- PAAWL/keymap.c
+++ PAAWL/keymap.c
@@ -170,8 +170,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if ((record->event.key.row == 5 && record->event.key.col == 0) ||
     (record->event.key.row == 11 && record->event.key.col == 5)) {
     if (record->event.pressed) {
-      layer_state_set(remove_auto_mouse_layer(layer_state, true));
+      state = remove_auto_mouse_layer(layer_state, true)
       set_auto_mouse_enable(false);
+      layer_state_set(state);
     } else {
       set_auto_mouse_enable(true);
     }
